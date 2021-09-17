22643520930d716cd89b46779f2e0c55f0c612e8
nick22morris <nick88morris22@gmail.com>
2021-09-17 18:56:58 -0500

    completed website

--- Kraftway/Runner/ContentView.swift
+++ Kraftway/Runner/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Kraftway/Views/HomeView.swift
+++ Kraftway/Views/HomeView.swift
@@ -9,109 +9,17 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ScrollView {
+        ScrollView(.vertical) {
             Image("logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 350, alignment: .center)
-                .padding(50)
-            ZStack {
-                Image("home")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: .infinity, alignment: .center)
-                    .blur(radius: 3.0)
-                VStack {
-                    Text("Local Neighborhood gathering spot")
-                        .bold()
-                        .font(.caption)
-                        .textCase(.uppercase)
-                        .foregroundColor(.white)
-                        .padding()
-                    Spacer()
-                    Text("Your home \naway from home.")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 400, alignment: .center)
-                    Spacer()
-                    Text("5809 PRESTON RD – STE 578\nPLANO, TX 75093")
-                        .bold()
-                        .font(.caption)
-                        .textCase(.uppercase)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(30)
-                }
-            }
-            //.padding(30)
-            Divider()
-            HStack {
-                VStack{
-                    Text("CRAVEABLE DISHES")
-                        .bold()
-                        .font(.caption)
-                        .textCase(.uppercase)
-                        .padding(.bottom, 20)
-                        .multilineTextAlignment(.center)
-                        .padding(.leading, 50)
-                    Text("Enjoy a Bite")
-                        .font(.title)
-                        .padding(.bottom, 20)
-                        .multilineTextAlignment(.center)
-                        .padding(.leading, 50)
-                    Text("CraftWay Kitchen features great food with a weekend brunch and a full bar with premium cocktails. Our team is full of passion and dedicated to delivering the perfect experience.")
-                        .frame(width: 175, height: 100)
-                        .multilineTextAlignment(.center)
-                            .lineLimit(7)
-                        .font(.custom("arial black", size: 12))
-                        .padding(.leading, 50)
-                        
-                }
-                Image("sandwich")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 25.0))
-                    .frame(width: 170, height: 100, alignment: .leading)
-                    .padding(40)
-            }
-            //.padding(30)
-            Divider()
-            ZStack{
-                Image("people")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: .infinity, alignment: .center)
-                    .blur(radius: 3.0)
-                VStack {
-                    Text("PROFESSIONALLY CASUAL")
-                        .bold()
-                        .font(.caption)
-                        .textCase(.uppercase)
-                        .foregroundColor(.white)
-                        .padding()
-                    Spacer()
-                    Text("Good Food.\nGood Friends.")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                    Spacer()
-                    Text("A favorite spot with the locals, expect a lively, bustling atmosphere each evening. Local artwork, open kitchen and a welcoming staff greet you at the door. It’s our craveable food that keeps our regulars coming back for more.")
-                        .bold()
-                        .font(.caption)
-                        .textCase(.uppercase)
-                        .foregroundColor(.white)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                        .multilineTextAlignment(.center)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .frame(width: 400, height: 100, alignment: .center)
-                        
-                    Spacer()
-                }
-            }
+                .padding(30)
+            bottomPic(image: "home", top: "Local Neighborhood gathering spot", middle: "Your home \naway from home.", bottom: "5809 PRESTON RD – STE 578\nPLANO, TX 75093")
+            sidePic(minor: "CRAVEABLE DISHES", title: "Enjoy a Bite", bottom: "CraftWay Kitchen features great food with a weekend brunch and a full bar with premium cocktails. Our team is full of passion and dedicated to delivering the perfect experience.", image: "sandwich")
+            bottomPic(image: "people", top: "PROFESSIONALLY CASUAL", middle: "Good Food.\nGood Friends.", bottom: "A favorite spot with the locals, expect a lively, bustling atmosphere each evening. Local artwork, open kitchen and a welcoming staff greet you at the door. It’s our craveable food that keeps our regulars coming back for more.")
+            sidePic(minor: "CRAFT COCKTAILS", title: "Take a Sip", bottom: "Built-by-hand cocktails with the finest ingredients curated by our in house drink experts.", image: "cock")
+            bottomPic(image: "tuna", top: "FOOD IS LOVE", middle: "Come Hungry.\nLeave Happy.", bottom: "Start with high-quality ingredients and focus on letting those ingredients shine. We want to share our passion and welcome you to our home to enjoy warm hospitality and amazing food. From simple but always satisfying roasted tomato soup to a hearty ribeye, ")
         }
     }
 }
@@ -119,5 +27,86 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+
+struct sidePic: View {
+    let minor: String
+    let title: String
+    let bottom: String
+    let image: String
+    var body: some View {
+        HStack {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                .frame(width: 170, height: 100, alignment: .trailing)
+                .padding()
+            VStack{
+                Text(minor)
+                    .bold()
+                    .font(.caption)
+                    .textCase(.uppercase)
+                    .padding(.bottom, 20)
+                    .multilineTextAlignment(.center)
+                    .padding(.trailing, 50)
+                Text(title)
+                    .font(.title)
+                    .padding(.bottom, 20)
+                    .multilineTextAlignment(.center)
+                    .padding(.trailing, 50)
+                Text(bottom)
+                    .frame(width: 175, height: 100)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(7)
+                    .font(.custom("arial black", size: 12))
+                    .padding(.trailing, 50)
+                
+            }
+            
+        }
+    }
+}
+
+struct bottomPic: View {
+    let image: String
+    let top: String
+    let middle: String
+    let bottom: String
+    var body: some View {
+        ZStack{
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: .infinity, alignment: .center)
+                .blur(radius: 3.0)
+            Color(.black)
+                .opacity(0.6)
+            VStack {
+                Text(top)
+                    .bold()
+                    .font(.caption)
+                    .textCase(.uppercase)
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                Text(middle)
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 400, alignment: .center)
+                Spacer()
+                Text(bottom)
+                    .bold()
+                    .font(.caption)
+                    .textCase(.uppercase)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(30)
+            }
+        }
     }
 }
